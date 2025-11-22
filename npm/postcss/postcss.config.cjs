d95743718394e2df6baf84765f379739017bdefa
LerpanWortt <lerpanwortt@gmail.com>
2025-11-22 11:10:51 +0100

    fix incorrect handling of prefix transformation with postcss

--- npm/postcss/postcss.config.cjs
+++ npm/postcss/postcss.config.cjs
@@ -1,5 +1,10 @@
 module.exports = {
     plugins: [
-        require("postcss-prefix-selector")({ prefix: ".__qyuddn" })
+        require("postcss-prefix-selector")({
+            prefix: ".__qyuddn",
+            transform: (prefix, selector) => {
+                return `${prefix}${selector}`
+            }
+        })
     ],
 }
